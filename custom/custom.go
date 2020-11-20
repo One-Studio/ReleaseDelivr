774bfe324706081bfef394d18b59f6e2eb1fa99f
Purp1e <47248616+Purple-CSGO@users.noreply.github.com>
2020-11-20 16:54:43 +0800

    Update custom.go

--- custom/custom.go
+++ custom/custom.go
@@ -1,52 +1,57 @@
 package custom
 
 import (
-	"encoding/json"
 	"errors"
 	"github.com/One-Studio/ReleaseDelivr/util"
+	"regexp"
+	"strings"
 )
-
-//利用 https://www.sojson.com/json/json2go.html
-//把对应json格式转换成golang的如下格式
-type Custom struct {
-	Name     string `json:"name"`
-	Type     string `json:"type"`
-	Version  string `json:"version"`
-	Size     int    `json:"size"`
-	Download struct {
-		SevenZ struct {
-			URL  string `json:"url"`
-			Size int    `json:"size"`
-			Sig  string `json:"sig"`
-		} `json:"7z"`
-		Zip struct {
-			URL  string `json:"url"`
-			Size int    `json:"size"`
-			Sig  string `json:"sig"`
-		} `json:"zip"`
-	} `json:"download"`
-}
 
 //利用获得上述json的链接，DIY获取版本号和下载链接 (version, link, err)
 func GetVersionAndLink(api string) (string, string, error) {
 	//固定：获取api的内容
+	count := 0
+	version, link := "", ""
 	content, err := util.GetHttpData(api)
+	for ; err != nil && count < 2; count++ {
+		content, err = util.GetHttpData(api)
+	}
 	if err != nil {
 		return "", "", err
 	}
 
-	//固定：初始化实例并解析JSON
-	var CustomInst Custom
-	err = json.Unmarshal([]byte(content), &CustomInst) //第二个参数要地址传递
-	if err != nil {
-		return "", "", err
+	//匹配正则表达式获得版本号
+	r := regexp.MustCompile(">(x265-Yuuki-\\S+.7z)<")
+	t := r.FindAllStringSubmatch(content, -1)
+
+	var files []string
+	for _, v := range t {
+		if len(v) > 0 {
+			files = append(files, v[1])
+		}
 	}
 
-	//固定：过滤失败的情况
-	if util.IsEmpty(CustomInst.Version) || util.IsEmpty(CustomInst.Download.SevenZ.URL) {
-		return "", "", errors.New("can't get version or download link")
-	} else {
-		//修改：处理得到版本号和链接
-		return CustomInst.Version, CustomInst.Download.SevenZ.URL, nil
+	if len(files) == 0 {
+		return "", "", errors.New("can't get target file names")
 	}
+
+	var latest string
+	latest = files[0]
+	for _, file := range files {
+		if strings.Compare(file, latest) == 1 {
+			latest = file
+		}
+	}
+
+	link = "https://down.7086.in/x265-Yuuki-Asuna/" + latest
+
+	r = regexp.MustCompile("x265-Yuuki-([\\S]+).7z")
+	res := r.FindStringSubmatch(latest)
+	if len(t) > 0 {
+		version = res[1]
+	}
+
+	return version, link, nil
+	//修改：处理得到版本号和链接
+	//return "", "", errors.New("can't get version, data: " + content)
 }
